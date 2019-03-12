c1eac8d61feb1666a50afa6104a69551b7f9a92b
Joubert Lacerda <46083794+JoubertLacerda@users.noreply.github.com>
2019-03-12 15:47:56 +1000

    Add files via upload

---
+++ MergeSort algorithm/MergeSort algorithm/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace MergeSort_algorithm
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(3, 48);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 0;
+            this.btnExecute.Text = "&Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(84, 48);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(423, 201);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(432, 255);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 285);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnExecute);
+            this.Name = "MainFrm";
+            this.Text = "MergeSort Algorithm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ MergeSort algorithm/MergeSort algorithm/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MergeSort_algorithm
+{
+    public partial class MainFrm : Form
+    {
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+        string UserArrayLength;
+        int num;
+        string UserArrayNo;
+        int ArrayValue;
+      
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+         //user sets length of the array.   
+            UserArrayLength = Microsoft.VisualBasic.Interaction.InputBox("Enter a number to determine the length of the array", "Enter Number");
+            try
+            {
+                // checks to see if user tried to pass a string, if true will fail.
+                while (!(int.TryParse(UserArrayLength, out num)))
+                {
+                    //user tries again.
+                    MessageBox.Show("Not a valid number, try again.");
+                    UserArrayLength = Microsoft.VisualBasic.Interaction.InputBox("Enter a number a to determine the length of the array", "Enter Number");
+                }
+            }
+            catch (Exception )
+            {
+                MessageBox.Show("Value entered is not in a valid format");
+            }
+
+            int intlength = Int32.Parse(UserArrayLength);
+            string[] stringsArray = new string[intlength];
+
+            // For display 
+            int ArrayPOS = 0; // set position of start of the array.
+            int POSvalue = 1; // set start value of the array.
+            txtOutput.Text += "Unsorted array: \r\n";
+            foreach (string s in stringsArray)
+            {
+                //user inputs the values to be placed inside the Array. values get placed one by one into position to be sorted after,displays it to user as its been entered.
+                string ArrayValue = Microsoft.VisualBasic.Interaction.InputBox("Enter a number for array value" + POSvalue, "Enter Number");
+                string arrayvalues = ArrayValue;
+                stringsArray[ArrayPOS] = arrayvalues;//.ToString();
+
+                txtOutput.Text += arrayvalues + "\t";
+                ArrayPOS++;
+                POSvalue++;
+            }
+            
+            mergeSort(stringsArray, 0, stringsArray.Length - 1);
+
+            txtOutput.Text += "\r\nSorted array: \r\n";
+            foreach (string i in stringsArray) // displays each values after its been sorted.
+            {
+                txtOutput.Text += i + "\t";
+            }
+            foreach(string s in stringsArray) // displays each step of the process
+            {
+                txtOutput.Text += ("\r\n Value at position" + " " + Array.IndexOf(stringsArray, s) + " " + "is " + " " + s + "\r\n");
+            }
+
+        }
+
+        public void mergeSort(string[] sortArray, int lower, int upper) // this mergeSort was modified to pass a string instead of an Int.
+        {
+            int middle;
+            if (upper == lower)
+                return;
+            else
+            {
+                middle = (lower + upper) / 2;
+                mergeSort(sortArray, lower, middle);
+                mergeSort(sortArray, middle + 1, upper);
+                Merge(sortArray, lower, middle + 1, upper);
+            }
+        }
+        public void Merge(string[] sortArray, int lower, int middle, int upper)// this Merge method was modified to work with a string instead of Int.
+        {
+            string[] temp = new string[sortArray.Length];
+            int lowEnd = middle - 1;
+            int low = lower;
+            int n = upper - lower + 1;
+            while ((lower <= lowEnd) && (middle <= upper))
+            {
+                if (sortArray[lower].CompareTo(sortArray[middle])<1)
+                {
+                    temp[low] = sortArray[lower];
+                    low++;
+                    lower++;
+                }
+                else
+                {
+                    temp[low] = sortArray[middle];
+                    low++;
+                    middle++;
+                }
+            }
+            while (lower <= lowEnd)
+            {
+                temp[low] = sortArray[lower];
+                low++;
+                lower++;
+            }
+            while (middle <= upper)
+            {
+                temp[low] = sortArray[middle];
+                low++;
+                middle++;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                sortArray[upper] = temp[upper];
+                upper--;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
